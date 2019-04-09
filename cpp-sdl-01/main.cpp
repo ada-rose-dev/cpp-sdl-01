505da02265cc892578bda9cfcf66c614ce36e177
PhoenixMandala <nyx.mandala@gmail.com>
2019-04-09 13:17:37 -0500

    Keyboard events. Lesson 3.

--- cpp-sdl-01/main.cpp
+++ cpp-sdl-01/main.cpp
@@ -3,13 +3,21 @@
 using namespace std;
 
 #pragma region Initalize SDL
-//const
-const int SCR_WIDTH = 640;
-const int SCR_HEIGHT = 480;
+
+//enums
+enum KeySurf {
+	KS_NONE,
+	KS_UP,
+	KS_DOWN,
+	KS_LEFT,
+	KS_RIGHT,
+	KS_COUNT
+};
 
 //globals - to be removed later on
 SDL_Window* gWin = nullptr;
 SDL_Surface* gScrSurf = nullptr; //Note: Surfaces use the CPU to render. Future tutorials will cover GPU rendering.
+SDL_Surface* gKeySurfs[KS_COUNT];
 SDL_Surface* gMedia = nullptr;
 
 //declare functions
@@ -19,6 +27,10 @@ bool loadMedia();
 
 #pragma region Functions
 bool init() {
+	//const
+	const int SCR_WIDTH = 640;
+	const int SCR_HEIGHT = 480;
+
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		printf("SDL Err: %s\n", SDL_GetError());
@@ -40,13 +52,20 @@ bool init() {
 	return true;
 }
 void close() {
-	//End the program
+	//Free surfaces
 	SDL_FreeSurface( gMedia );
-	gMedia = NULL;
+	gMedia = nullptr;
 
+	for (int i = 0; i < KS_COUNT; i++){
+		SDL_FreeSurface(gKeySurfs[i]);
+		gKeySurfs[i] = nullptr;
+	}
+
+	//Destroy window
 	SDL_DestroyWindow( gWin );
-	gWin = NULL;
+	gWin = nullptr;
 
+	//Quit SDL
 	SDL_Quit();
 }
 bool loadMedia() {
@@ -56,11 +75,27 @@ bool loadMedia() {
 		printf("SDL Err: %s\n", SDL_GetError());
 		return false;
 	}
+
+	//Load keypress images
+	gKeySurfs[KS_NONE] = SDL_LoadBMP("./img/press.bmp");
+	gKeySurfs[KS_UP] = SDL_LoadBMP("./img/up.bmp");
+	gKeySurfs[KS_DOWN] = SDL_LoadBMP("./img/down.bmp");
+	gKeySurfs[KS_LEFT] = SDL_LoadBMP("./img/left.bmp");
+	gKeySurfs[KS_RIGHT] = SDL_LoadBMP("./img/right.bmp");
+	//this isn't ideal. will alert the last loaded error. so if there are multiple you won't be able to tell.
+	for (int i = 0; i < KS_COUNT; i++) {
+		if (gKeySurfs[i] == nullptr) {
+			printf("SDL Err: %s\n", SDL_GetError());
+			return false;
+		}
+	}
+
+
 	return true;
 }
 
-void render() {
-	SDL_BlitSurface(gMedia, nullptr, gScrSurf, nullptr);
+void render(SDL_Surface* surf) {
+	SDL_BlitSurface(surf, nullptr, gScrSurf, nullptr);
 	SDL_UpdateWindowSurface(gWin);
 }
 /*
@@ -90,6 +125,9 @@ int main(int argc, char* argv[]) {
 			bool run = true;
 			SDL_Event e;
 
+			//Set default surface
+			SDL_Surface* gCurrentSurf = gKeySurfs[KS_NONE];
+
 			while (run) {
 				//SDL_Event handler
 				//run = eventHandler(e);
@@ -98,11 +136,33 @@ int main(int argc, char* argv[]) {
 					case SDL_QUIT:
 						run = false;
 						break;
+
+					case SDL_KEYDOWN:
+						switch (e.key.keysym.sym)
+						{
+							case SDLK_UP:
+								gCurrentSurf = gKeySurfs[KS_UP];
+								break;
+							case SDLK_DOWN:
+								gCurrentSurf = gKeySurfs[KS_DOWN];
+								break;
+							case SDLK_LEFT:
+								gCurrentSurf = gKeySurfs[KS_LEFT];
+								break;
+							case SDLK_RIGHT:
+								gCurrentSurf = gKeySurfs[KS_RIGHT];
+								break;
+						}
+						break;
+
+					case SDL_KEYUP: //Not the best way to achieve this effect. Use key states (lesson 18)
+						gCurrentSurf = gKeySurfs[KS_NONE];
+						break;
 					}
 				}
 
 				//Render
-				render();
+				render(gCurrentSurf);
 			}
 		}
 	}
